a2280abf616f59ac5cf06767ed1f7b7a58fe95ae
Savya Vats <68233028+vatssavyya@users.noreply.github.com>
2026-07-10 10:59:09 -0400

    checking JSON structure

--- testScript.py
+++ testScript.py
@@ -3,7 +3,13 @@ import sqlite3
 conn = sqlite3.connect("data/processed/vgc.db")
 cur = conn.cursor()
 
-cur.execute("SELECT DISTINCT category FROM battle_stats")
+cur.execute("""
+    SELECT rank, hp_points, attack_points, defense_points,
+           sp_atk_points, sp_def_points, speed_points, percentage
+    FROM battle_stats
+    WHERE pokemon = 'Garchomp' AND category = 'stat_points'
+    ORDER BY rank
+""")
 for row in cur.fetchall():
     print(row)
 
